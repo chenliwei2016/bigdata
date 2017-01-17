fb75cfd1a84b4d4abd00559368314095c3609d54
chenliwei2016 <mickk@qq.com>
2017-01-18 07:10:13 +0800

    javacore

---
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/App.java
@@ -0,0 +1,13 @@
+package win.chenliwei.javacore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/InnerClassTest.java
@@ -0,0 +1,38 @@
+package win.chenliwei.javacore;
+
+
+interface Car{
+	void run();
+	void stop();
+}
+
+interface WarCar extends Car{
+	void fire();
+}
+
+class Men{
+	public void fuck(String who){
+		System.out.println("fuck " + who);
+	}
+}
+
+public class InnerClassTest {
+	public static void main(String args[]){
+		WarCar warcar = new WarCar(){
+			public void fire(){System.out.println("fire from WarCar");}
+			public void run(){System.out.println("run from Car");}
+			public void stop(){System.out.println("stop from Car");}
+			
+		};
+		warcar.fire();
+		Men zhangpeng = new Men(){
+			public void fuck(String who){
+				super.fuck(who);
+				System.out.println("fuck " + who + " again！");
+			}
+		};
+		zhangpeng.fuck("苍井空");
+		
+	}
+
+}
