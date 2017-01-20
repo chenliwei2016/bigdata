c10e24c05f95f386325ed6ef3a9fc89881c4d75d
hadoop <hadoop@bigdata1.(none)>
2017-01-20 15:19:46 +0800

    update oobase

---
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/oobasic/VisitOtherObjectPrivateVars.java
@@ -0,0 +1,29 @@
+package win.chenliwei.javacore.oobasic;
+
+public class VisitOtherObjectPrivateVars {
+
+	public static void main(String[] args) {
+		Employee zhangpeng = new Employee("zhangpeng",23,"male",50000);
+		Employee yaoxiang = new Employee("yaoxiang",28,"male",200);
+		System.out.println(zhangpeng.seeOthersSalary(yaoxiang));
+	}
+
+}
+
+@SuppressWarnings("unused")
+class Employee{
+	private String name;
+	private int age;
+	private String sex;
+	private double saraly;
+	public Employee(String name, int age, String sex, double saraly) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.sex = sex;
+		this.saraly = saraly;
+	}
+	public double seeOthersSalary(Employee employee){
+		return employee.saraly;
+	}
+}
