d473393b0968c4b53058c378254c8ab87be91791
leavy <leavy@192.168.56.1>
2017-03-02 13:20:25 +0800

    Updated

--- JavaCore/javacore/src/main/java/win/chenliwei/javacore/setclass/CollectionIteratorTest.java
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/setclass/CollectionIteratorTest.java
@@ -6,12 +6,16 @@
  * 	1. Collection, it is one of basic interface for all collection types
  * 	2. Iterator, another basic interface for them
  * Let's learn the basic usage of those 2 interfaces
+ * In the end, practice some static function of Collections class, which implemented
+ * algorithms of sort,search
  */
 package win.chenliwei.javacore.setclass;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 public class CollectionIteratorTest {
 
@@ -36,8 +40,36 @@ public class CollectionIteratorTest {
 		it.next(); // returns zhangpeng
 		it.remove(); // OK, remove zhangpeng
 		for(Customer customer : c) System.out.println(customer.getName());
-		it.remove(); //Error, because the item of last next() has been removed already
-
+		//it.remove(); //Error, because the item of last next() has been removed already
+		
+		/*
+		 * Let's practice some static functions from Collections
+		 */
+		
+		List<Integer> a = new ArrayList<Integer>(), b = new ArrayList<Integer>();
+		a.add(1);a.add(2);a.add(3);a.add(10);a.add(7);a.add(8);a.add(9);a.add(5);a.add(4);
+		//Copy the content from one list to another, not good to use in real world because
+		//it needs to initialize destination and guarantee the size() is identical
+		b.addAll(a);
+		Collections.copy(b, a); //So, we use addAll instead of copy
+		System.out.println("*******The list a*********");
+		a.forEach(i->{System.out.print(i + ",");});
+		System.out.print("\n");
+		System.out.println("The max value of list a is :" + Collections.max(a)); //Max function
+		Collections.sort(a); // get sorted
+		System.out.println("*******The sorted list a*********");
+		a.forEach(i->{System.out.print(i + ",");});
+		System.out.print("\n");
+		System.out.println("The location of 10 in list a is : " + Collections.binarySearch(a, 5));
+		Collections.reverse(a);
+		System.out.println("*******The reversed list a*********");
+		a.forEach(i->{System.out.print(i + ",");});
+		System.out.print("\n");
+		Collections.rotate(a, 2);
+		System.out.println("*******The rotated list a with distance 2*********");
+		a.forEach(i->{System.out.print(i + ",");});
+		System.out.print("\n");
+		
 	}
 
 }
