d40b6aea89f67cdaa79748b6b49cc933690d5a9f
leavy <leavy@192.168.56.1>
2017-03-07 14:52:35 +0800

    Revised by GuPei for code review

--- JavaCore/javacore/src/main/java/win/chenliwei/javacore/multithread/CountDownLatchTest.java
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/multithread/CountDownLatchTest.java
@@ -1,3 +1,5 @@
+package win.chenliwei.javacore.multithread;
+
 /**
  * @author: ChenLiwei
  * 2017-03-06
@@ -8,9 +10,13 @@
  * then, the red packets sent out, all of them includes you can open it with your share.
  * when all the packets are robbed out, we should set the packet as an empty one and tell the
  * following people not to rob again.
+ * 
+ * Code review by GuPei:
+ * 	1. Use parallelStream for parallel visit a set and it automatically implements common fork-join
+ * 		so no need to use either Runnable or CountDownLatch anymore, this is Java 8 feature
+ * 	2. Move the local inner class out of main function, this is the least suggested way, should avoid
+ * 	3. Try to make the main function clean and clear, to highlight the main idea stream.
  */
-package win.chenliwei.javacore.multithread;
-
 import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,118 +28,113 @@ import java.util.concurrent.FutureTask;
 
 public class CountDownLatchTest {
 
-	public static void main(String[] args) throws InterruptedException {
-		long current;
-		Set<String> wechat = new HashSet<>();
-		wechat.add("chenliwei");
-		wechat.add("zhangpeng");
-		wechat.add("yaoxiang");
-		wechat.add("zhangwei1");
-		wechat.add("zhangwei2");
-		RedPacket packet = null;
-		CountDownLatch waitRedPacket = new CountDownLatch(1);
-		class preparePacket implements Callable<RedPacket>{
-			private CountDownLatch cdl;
-			private String owner;
-			private double amount;
-			private int share;
-			
-			public preparePacket(CountDownLatch cdl, String owner, double amount, int share) {
-				this.cdl = cdl;
-				this.owner = owner;
-				this.amount = amount;
-				this.share = share;
-			}
+    public static void main(String[] args) throws InterruptedException {
+        long current;
+        Set<String> wechat = new HashSet<>();
+        wechat.add("chenliwei");
+        wechat.add("zhangpeng");
+        wechat.add("yaoxiang");
+        wechat.add("zhangwei1");
+        wechat.add("zhangwei2");
+        final RedPacket packet = getPreparePacket();
 
-			@Override
-			public RedPacket call() throws Exception {
-				System.out.println(owner + "'s red packet will send out in 1 second, Ready!");
-				Thread.sleep(1000);
-				RedPacket packet = new RedPacket(cdl, owner, amount, share);
-				cdl.countDown();
-				return packet;
-			}
-			
-		}
-		try {
-			FutureTask<RedPacket> prepare = new FutureTask<>(new preparePacket(waitRedPacket,"chenliwei",100,4));
-			new Thread(prepare).start();
-			packet = prepare.get();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
-		waitRedPacket.await();
-		current = System.currentTimeMillis();
-		CountDownLatch  waitRobOut = new CountDownLatch(wechat.size());
-		class RobPacket implements Runnable{
-			private CountDownLatch cdl;
-			private RedPacket packet;
-			private String name;
-			
-			public RobPacket(CountDownLatch cdl, RedPacket packet, String name) {
-				this.cdl = cdl;
-				this.packet = packet;
-				this.name = name;
-			}
+        current = System.currentTimeMillis();
+        wechat.parallelStream()
+            .forEach( name->  {
+                try {
+                    Thread.sleep((long)(2000 * Math.random()));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                packet.luckyHand(name);
+            } );
 
-			@Override
-			public void run() {
-				try {
-					Thread.sleep((long)(2000 * Math.random()));
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				packet.luckyHand(name);
-				cdl.countDown();
-			}
-		}
-		for(String member : wechat) new Thread(new RobPacket(waitRobOut,packet, member)).start();
-		waitRobOut.await();
-		System.out.println("All packets are robbed out in " + (int)((System.currentTimeMillis() - current)/1000) + " seconds");
-		packet.getDetail();
-	}
+        System.out.println("All packets are robbed out in " + (int)((System.currentTimeMillis() - current)/1000) + " seconds");
+        packet.getDetail();
+    }
+
+    public static RedPacket getPreparePacket() throws InterruptedException{
+
+        RedPacket packet = null;
+        CountDownLatch waitRedPacket = new CountDownLatch(1);
+
+        try {
+            FutureTask<RedPacket> prepare = new FutureTask<>(new preparePacket(waitRedPacket,"chenliwei",100,4));
+            new Thread(prepare).start();
+            packet = prepare.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        waitRedPacket.await();
+        return packet;
+
+    }
 
 }
 
 
+class preparePacket implements Callable<RedPacket>{
+    private CountDownLatch cdl;
+    private String owner;
+    private double amount;
+    private int share;
+
+    public preparePacket(CountDownLatch cdl, String owner, double amount, int share) {
+        this.cdl = cdl;
+        this.owner = owner;
+        this.amount = amount;
+        this.share = share;
+    }
+
+    @Override
+    public RedPacket call() throws Exception {
+        System.out.println(owner + "'s red packet will send out in 1 second, Ready!");
+        Thread.sleep(1000);
+        RedPacket packet = new RedPacket( owner, amount, share);
+        cdl.countDown();
+        return packet;
+    }
+
+}
+
 class RedPacket{
-	private String owner;
-	private double amount;
-	private int share;
-	private ConcurrentSkipListMap<Integer,String> detail;
-	public RedPacket(CountDownLatch cdl,String owner, double amount, int share) throws InterruptedException {
-		this.owner = owner;
-		this.amount = amount;
-		this.share = share;
-		this.detail = new ConcurrentSkipListMap<Integer,String>();
-	}
-	
-	public String getOwner() {
-		return owner;
-	}
+    private String owner;
+    private double amount;
+    private int share;
+    private ConcurrentSkipListMap<Integer,String> detail;
+    public RedPacket(String owner, double amount, int share) throws InterruptedException {
+        this.owner = owner;
+        this.amount = amount;
+        this.share = share;
+        this.detail = new ConcurrentSkipListMap<Integer, String>();
+    }
 
-	public int getShare() {
-		return share;
-	}
+    public String getOwner() {
+        return owner;
+    }
 
-	public void getDetail() {
-		System.out.println("Look lucky hand of " + owner + "'s red packet");
-		detail.forEach((k,v) -> {System.out.println(v);});
-	}
-	
-	public synchronized void luckyHand(String name){
-		if(share == 0) return;
-		DecimalFormat  df = new DecimalFormat("######0.00"); 
-		if(share == 1) {
-			detail.put(share, name + " got $" + df.format(amount));
-			share = 0;
-			amount = 0;
-		}else{
-			double hand = amount * Math.random();
-			if (hand < 0.01) hand = 0.01;
-			detail.put(share, name + " got $" + df.format(hand));
-			amount -= hand;
-			share--;
-		}
-	}
+    public int getShare() {
+        return share;
+    }
+
+    public void getDetail() {
+        System.out.println("Look lucky hand of " + owner + "'s red packet");
+        detail.forEach( (k,v) -> System.out.println(v));
+    }
+
+    public synchronized void luckyHand(String name){
+        if(share == 0) return;
+        DecimalFormat  df = new DecimalFormat("######0.00");
+        if(share == 1) {
+            detail.put(share, name + " got $" + df.format(amount));
+            share = 0;
+            amount = 0;
+        }else{
+            double hand = amount * Math.random();
+            if (hand < 0.01) hand = 0.01;
+            detail.put(share, name + " got $" + df.format(hand));
+            amount -= hand;
+            share--;
+        }
+    }
 }
