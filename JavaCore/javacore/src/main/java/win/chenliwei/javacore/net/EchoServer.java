0a3d3a327da5cd1670ba4f0f76cce2b24d100d8d
leavy <leavy@192.168.56.1>
2017-03-16 11:32:29 +0800

    provide multiply users to connect

--- JavaCore/javacore/src/main/java/win/chenliwei/javacore/net/EchoServer.java
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/net/EchoServer.java
@@ -6,6 +6,7 @@
  * Guide to run this program:
  * 1. run the java
  * 2. telent localhost:8189
+ * 3. Manually stop the server if you don't want to provide service any longer
  */
 package win.chenliwei.javacore.net;
 
@@ -21,26 +22,49 @@ public class EchoServer {
 
 	public static void main(String[] args) throws IOException {
 		try(ServerSocket server = new ServerSocket(8189)){
-			try(Socket incoming = server.accept()){
-				InputStream inStream = incoming.getInputStream();
-				OutputStream outStream = incoming.getOutputStream();
-				try(Scanner in = new Scanner(inStream)){
-					PrintWriter out = new PrintWriter(outStream,true);
-					out.println("You connected to the server, Enter Bye to exit");
-					boolean done = false;
-					while(in.hasNextLine() && !done){
-						String line = in.nextLine();
-						if(line.trim().equalsIgnoreCase("bye")){
-							done = true;
-							out.println("Good Bye!");
-							out.close();
-						} else {
-							out.println("Echo: " + line);
-						}
-					}
-				}
+			int i = 0;
+			while(true){
+					Socket incoming = server.accept();
+					System.out.println("The client " + (++i) + " connected");
+					Runnable r = new EchoHandler(incoming);
+					new Thread(r).start();
 			}
 		}
 	}
 
+}
+
+class EchoHandler implements Runnable{
+	private Socket incoming;
+
+	public EchoHandler(Socket incoming) {
+		this.incoming = incoming;
+	}
+
+	@Override
+	public void run() {
+		InputStream inStream;
+		try {
+			inStream = incoming.getInputStream();
+			OutputStream outStream = incoming.getOutputStream();
+			try(Scanner in = new Scanner(inStream)){
+				PrintWriter out = new PrintWriter(outStream,true);
+				out.println("You connected to the server, Enter Bye to exit");
+				boolean done = false;
+				while(in.hasNextLine() && !done){
+					String line = in.nextLine();
+					if(line.trim().equalsIgnoreCase("bye")){
+						done = true;
+						out.println("Good Bye!");
+						incoming.close();
+					} else {
+						out.println("Echo: " + line);
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
