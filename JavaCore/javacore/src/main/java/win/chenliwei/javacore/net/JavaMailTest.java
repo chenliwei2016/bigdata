4261127ffcecf0362af5ab21bd5c0990d469c733
chenliwei2016 <mickk@qq.com>
2017-03-18 17:20:15 +0800

    Workable Java Mail with hotmail

--- JavaCore/javacore/src/main/java/win/chenliwei/javacore/net/JavaMailTest.java
+++ JavaCore/javacore/src/main/java/win/chenliwei/javacore/net/JavaMailTest.java
@@ -3,62 +3,36 @@
  * 2017年3月18日
  * JavaMailTest.java
  * Comments: It is to demonstrate how to use java mail API to send mails to others
+ * Since I am in China, cannot access gmail directly, luckily, hotmail can help the test.
+
  */
 package win.chenliwei.javacore.net;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Properties;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
-
-import javax.mail.Message.RecipientType;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 public class JavaMailTest {
 
-	public static void main(String[] args) throws IOException, MessagingException {
-		Properties pros = new Properties();
-		try(InputStream in = Files.newInputStream(Paths.get("mail.properties"))){
-			pros.load(in);
+	public static void main(String[] args) throws Exception {
+		String user,password;
+		user = args[0];
+		try(Scanner in = new Scanner(System.in)){
+			System.out.println("Enter the password for " + user);
+			password = in.nextLine();
 		}
-		List<String> message = Files.readAllLines(Paths.get(args[0]), Charset.forName("UTF-8"));
-		String from = message.get(0);
-		String to = message.get(1);
-		String subject = message.get(2);
-		StringBuilder text = new StringBuilder();
-		for(int i=3; i<message.size();i++){
-			text.append(message.get(i));
-			text.append("\n");
-		}
-		
-//		//Consider security, we need you input password
-//		Console console = System.console();
-//		String password = new String(console.readPassword("Please enter the password for the mail " + from));
-		Scanner in = new Scanner(System.in);
-		String password = in.nextLine();
-		in.close();
-		Session mailSession = Session.getDefaultInstance(pros);
-		MimeMessage mail = new MimeMessage(mailSession);
-		mail.setFrom(from);
-		mail.addRecipient(RecipientType.TO, new InternetAddress(to));
-		mail.setSubject(subject);
-		mail.setText(text.toString());
-		Transport tr = mailSession.getTransport();
-		try{
-			tr.connect(null, password);
-			tr.sendMessage(mail, mail.getAllRecipients());
-		} finally{
-			tr.close();
-		}
-		
+		String nickName = "";
+		Mail mail = new Mail(user,password);
+		try {  
+			nickName=javax.mail.internet.MimeUtility.encodeText("ChenLiWei");  
+		} catch (UnsupportedEncodingException e) {  
+			e.printStackTrace();  
+		}   
+		mail.setFrom(nickName+"<"+user+">"); //Notice Here, you need set a nickname
+		mail.setTo(new String[]{args[1]});
+		mail.setSubject("JavaMail Testing");
+		mail.setBody("Greeting from " + nickName + " using mailbox " + user);
+		mail.send();
+		System.out.println("The mail has been sent!");
 	}
 
 }
