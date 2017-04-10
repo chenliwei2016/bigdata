53a72e8aac6a8bb69a0f0235154ff1adae66f615
leavy <leavy@chenliwei>
2017-04-10 17:21:52 +0800

    Factory pattern initial

---
+++ JavaDesignPattern/src/main/java/win/chenliwei/designpattern/factory/factorymethod/Pizza.java
@@ -0,0 +1,10 @@
+package win.chenliwei.designpattern.factory.factorymethod;
+
+public class Pizza {
+	private String name;
+	private String dough;
+	private String sauce;
+	private ArrayList<String> toppings;
+	
+	
+}
